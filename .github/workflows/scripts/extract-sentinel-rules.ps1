09e1cb709a89a6bc8d106bd7289d5a863580e7bd
RixonTelus <rixon.polvi@telus.com>
2023-12-20 02:14:59 -0500

    Changes

--- .github/workflows/scripts/extract-sentinel-rules.ps1
+++ .github/workflows/scripts/extract-sentinel-rules.ps1
@@ -1,15 +1,23 @@
 # Variables
+$currentDate = Get-Date -Format "yyyy-MM-dd"
+$exportPath = 'Export-' + $currentDate + '.json'
 
-Write-Host "test"
+Import-Module AzSentinel
+
+Export-AzSentinel -workspace $workspaceName -Outputfolder ".\" -Kind Alert
+
+Get-ChildItem -Filter "Alert*" -File | 
+   Rename-Item -NewName $exportPath 
+
 
 # Set the subscription context
 #Set-AzContext -SubscriptionId $subscriptionId
 
 # Get the list of Azure Sentinel analytic rules
-$rules = Get-AzSentinelAlertRule -ResourceGroupName $resourceGroupName -WorkspaceName $workspaceName -SubscriptionId $subscriptionId
+#$rules = Get-AzSentinelAlertRule -ResourceGroupName $resourceGroupName -WorkspaceName $workspaceName -SubscriptionId $subscriptionId
 
 # Export the rules to a JSON file
-$rules | ConvertTo-Json | Out-File -FilePath $ruleExportPath
+#$rules | ConvertTo-Json | Out-File -FilePath $ruleExportPath
 
 # Output the total number of rules extracted
-Write-Host "Total rules extracted: $($rules.Count)"
+#Write-Host "Total rules extracted: $($rules.Count)"
